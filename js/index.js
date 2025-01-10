565e3aa3216e88977d44e6f66bbe776d767e2c68
Rafael Fernandes <rf7223216@gmail.com>
2025-01-10 01:08:37 -0300

    muita coisa

--- js/index.js
+++ js/index.js
@@ -80,4 +80,6 @@ function toggleMenu() {
     const menuItems = document.getElementById("menu-items");
     menuItems.classList.toggle("active");
 }
-    
+function toggleDarkMode() {
+    document.documentElement.classList.toggle('dark-mode');
+}
