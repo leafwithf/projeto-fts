e03c7042b9034a424dba424a6bf16c3eda1f419d
Rafael Fernandes <rf7223216@gmail.com>
2025-02-05 19:24:30 -0300

    login

--- js/login.js
+++ js/login.js
@@ -1,15 +1,24 @@
-function togglePassword() {
-    const passwordField = document.getElementById('password');
-    const eyeIcon = document.getElementById('eye-icon');
+window.onload = function() {
+    // Função para alternar a visibilidade da senha
+    function togglePassword() {
+        const passwordField = document.getElementById('password');
+        const eyeIcon = document.getElementById('eye-icon');
 
-    const type = passwordField.type === 'password' ? 'text' : 'password';
-    passwordField.type = type;
+        const type = passwordField.type === 'password' ? 'text' : 'password';
+        passwordField.type = type;
 
-    if (type === 'password') {
-        eyeIcon.classList.remove('fa-eye');
-        eyeIcon.classList.add('fa-eye-slash');
-    } else {
-        eyeIcon.classList.remove('fa-eye-slash');
-        eyeIcon.classList.add('fa-eye');
+        if (type === 'password') {
+            eyeIcon.classList.remove('fa-eye');
+            eyeIcon.classList.add('fa-eye-slash');
+        } else {
+            eyeIcon.classList.remove('fa-eye-slash');
+            eyeIcon.classList.add('fa-eye');
+        }
     }
-}
+
+    // Agora você pode usar a função togglePassword para o ícone
+    const eyeIcon = document.getElementById('eye-icon');
+    if (eyeIcon) {
+        eyeIcon.addEventListener('click', togglePassword);
+    }
+};
