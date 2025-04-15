5c7850856a4ba7ce6b7f5a81a8fdef875c9fffa0
leaf <144567276+leafwithf@users.noreply.github.com>
2025-04-15 14:30:41 -0300

    Update logo icons for dark mode support and refactor related styles

--- src/scripts/pages/darkmode.js
+++ src/scripts/pages/darkmode.js
@@ -1,28 +1,35 @@
 function updateLogosForTheme(theme) {
-    const logoClasses = ['.footer-logo img', '.header-logo img'];
-    
-    logoClasses.forEach(selector => {
-      const logos = document.querySelectorAll(selector);
-      logos.forEach(logo => {
-        logo.src = theme === 'dark'
-          ? '../../public/assets/images/logo/logo-white.png'
-          : '../../public/assets/images/logo/logo-black.png';
-      });
+  const logoClasses = ['.footer-logo img', '.header-logo img'];
+
+  // Atualiza os logos
+  logoClasses.forEach(selector => {
+    const logos = document.querySelectorAll(selector);
+    logos.forEach(logo => {
+      logo.src = theme === 'dark'
+      ? '../../../public/assets/images/logo/logo-white.png'
+      : '../../../public/assets/images/logo/logo-black.png';
     });
-  }
-  
-  function applySystemThemePreference() {
-    const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
-    const theme = prefersDark ? 'dark' : 'light';
-    document.documentElement.setAttribute('data-theme', theme);
-    updateLogosForTheme(theme);
-  }
-  
-  window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
-    const newTheme = e.matches ? 'dark' : 'light';
-    document.documentElement.setAttribute('data-theme', newTheme);
-    updateLogosForTheme(newTheme);
   });
-  
-  document.addEventListener('DOMContentLoaded', applySystemThemePreference);
-  
+
+  const favicon = document.querySelector("link[rel~='icon']");
+  if (favicon) {
+    favicon.href = theme === 'dark'
+      ? '../../../public/assets/images/logo/logoic-white.ico' 
+      : '../../../public/assets/images/logo/logoic-black.ico'; 
+  }
+}
+
+function applySystemThemePreference() {
+  const prefersDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const theme = prefersDark ? 'dark' : 'light';
+  document.documentElement.setAttribute('data-theme', theme);
+  updateLogosForTheme(theme);
+}
+
+window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', (e) => {
+  const newTheme = e.matches ? 'dark' : 'light';
+  document.documentElement.setAttribute('data-theme', newTheme);
+  updateLogosForTheme(newTheme);
+});
+
+document.addEventListener('DOMContentLoaded', applySystemThemePreference);
